b767b4be6a43afdf3f7225548c0266eb334681d3
ZK Liu <zkliu@ZKs-Mac.local>
2018-11-19 14:59:39 -0800

    implement 1 random enemy

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,12 @@
-// start slingin' some d3 here.
+var svgWidth = 1200, svgHeight = 600;
+
+var board = d3.select('.board').append('svg:svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight);
+ 
+var circle = board.append('circle')
+  .attr('cx', Math.random()*svgWidth)
+  .attr('cy', Math.random()*svgHeight)
+  .attr('r', 10)
+  .attr('fill', 'red');
+
