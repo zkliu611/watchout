9ef1280e3468f6353839b9196f401bc27671161a
ZK Liu <zkliu@ZKs-Mac.local>
2018-11-19 15:19:07 -0800

    implement n number of enemies and player circle

--- client/watchout.js
+++ client/watchout.js
@@ -1,12 +1,23 @@
-var svgWidth = 1200, svgHeight = 600;
+var svgWidth = 1200, svgHeight = 600, enemiesCount = 30;
+
+// var enemiesData = _.range(enemiesCount);
 
 var board = d3.select('.board').append('svg:svg')
   .attr('width', svgWidth)
   .attr('height', svgHeight);
- 
-var circle = board.append('circle')
+
+for (let i = 0; i < enemiesCount; i++){
+  var enemyCircle = board.append('circle')
   .attr('cx', Math.random()*svgWidth)
   .attr('cy', Math.random()*svgHeight)
   .attr('r', 10)
   .attr('fill', 'red');
+
+}
+ 
+var playerCircle = board.append('circle')
+  .attr('cx', Math.random()*svgWidth)
+  .attr('cy', Math.random()*svgHeight)
+  .attr('r', 10)
+  .attr('fill', 'green');
 
